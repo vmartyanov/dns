cc5773172a31b84ad619572c2666a9e1f808b9e6
Vladimir Martianov <vilgeforce@gmail.com>
2026-09-03 15:36:04 +0200

    Added new record types

--- __init__.py
+++ __init__.py
@@ -35,20 +35,25 @@ DNSKEY      = 48
 NSEC3       = 50
 NSEC3PARAM  = 51
 TLSA        = 52
+CDS         = 59
+CDNSKEY     = 60
 OPENPGPKEY  = 61
 SPF         = 99
 AXFR        = 252
 CAA         = 257
 
 PRIV65534   = 65534
+
+
+#See https://en.wikipedia.org/wiki/List_of_DNS_record_types
 TYPES = {0: "UNKNOWN", 1: "A", 2: "NS", 3: "MD", 4: "MF", 5: "CNAME", 6: "SOA", 7: "MB", 8: "MG",
             9: "MR",
         10: "NULL", 11: "WKS", 12: "PTR", 13: "HINFO", 14: "MINFO", 15: "MX", 16: "TXT", 17: "RP",
         28: "AAAA", 29: "LOC",
         33: "SRV", 35: "NAPTR", 39: "DNAME",
         43: "DS", 44: "SSHFP", 46: "RRSIG", 47: "NSEC", 48: "DNSKEY",
-        50: "NSEC3", 51: "NSEC3PARAM", 52: "TLSA",
-        61: "OPENPGPKEY",
+        50: "NSEC3", 51: "NSEC3PARAM", 52: "TLSA", 59: "CDS",
+        60: "CDNSKEY", 61: "OPENPGPKEY",
         99: "SPF",
         252: "AXFR", 257: "CAA",
         65534: "PRIV65534"
